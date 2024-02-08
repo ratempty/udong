5e87079b5c1fa707b74a4ab6b69aa548de158a86
JY2345 <ilokids@gmail.com>
2024-02-08 16:24:15 +0900

    Style : 파일 병합, 프리티어

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -4,6 +4,139 @@ import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
+//게시글 등록
+router.post(
+	'/community/:communityId',
+
+	async (req, res, next) => {
+		const { communityId } = req.params;
+		// const { userId } = req.user;
+		const { title, content } = req.body;
+
+		//유저가 커뮤니티에 속해있으면 작성 아니면 모임가입필요
+
+		// const isCommunityUser = await prisma.communityUsers.findfirst({
+		//   where: {
+		//     userId: +userId,
+		//     communityId: +communityId,
+		//   },
+		// });
+
+		// if (isCommunityUser) {
+		//   const post = await prisma.posts.create({
+		//     data: {
+		//       userId: +userId,
+		//       communityId: +communityId,
+		//       title: title,
+		//       content: content,
+		//     },
+		//   });
+		// } else {
+		//   return res
+		//     .status(403)
+		//     .json({ message: "모임에 가입한 사람만 글을 작성할 수 있습니다." });
+		// }
+
+		const dd = await prisma.posts.create({
+			data: {
+				communityId: +communityId,
+				title: title,
+				content: content,
+				userId: 1,
+			},
+		});
+
+		return res.status(201).json({ message: '작성완료' });
+	},
+);
+
+// 게시글 수정
+// localhost:3000/api/:communityId/:postId
+router.patch(
+	'/:communityId/:postId',
+
+	async (req, res, next) => {
+		const { communityId, postId } = req.params;
+		const { userId } = req.user;
+		const { title, content } = req.body;
+
+		// 자기 글인지 확인
+		const isMyPost = await prisma.posts.findFirst({
+			where: {
+				communityId: +communityId,
+				id: +postId,
+			},
+			select: {
+				userId: true,
+			},
+		});
+
+		if (!isMyPost) {
+			return res.status(404).json({ message: '해당 글을 찾을 수 없습니다.' });
+		}
+
+		// if (isMyPost.userId !== userId) {
+		//   return res
+		//     .status(403)
+		//     .json({ message: "본인의 글만 수정할 수 있습니다." });
+		// }
+
+		const updatePost = await prisma.posts.update({
+			where: {
+				id: +postId,
+				communityId: +communityId,
+				userId: +userId,
+			},
+			data: {
+				title: title,
+				content: content,
+			},
+		});
+		return res.status(200).json({ message: '수정 완료' });
+	},
+);
+
+// 게시글 삭제
+//localhost:3000/api/:communityId/:postId
+router.delete(
+	'/:communityId/:postId',
+	authMiddleware,
+	async (req, res, next) => {
+		const { communityId, postId } = req.params;
+		const { userId } = req.user;
+
+		// 자기 글인지 확인
+		const isMyPost = await prisma.posts.findFirst({
+			where: {
+				communityId: +communityId,
+				id: +postId,
+			},
+			select: {
+				userId: true,
+			},
+		});
+
+		if (!isMyPost) {
+			return res.status(404).json({ message: '해당 글을 찾을 수 없습니다.' });
+		}
+
+		if (isMyPost.userId !== userId) {
+			return res
+				.status(403)
+				.json({ message: '본인의 글만 삭제할 수 있습니다.' });
+		}
+
+		await prisma.posts.delete({
+			where: {
+				communityId: +communityId,
+				id: +postId,
+				userId: +userId,
+			},
+		});
+		return res.status(204).json({ message: '삭제완료' });
+	},
+);
+
 /**
  * 댓글 등록
  */
