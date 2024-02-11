87211bda603e08216b115b4520dc6ea9ab5c7f29
JY2345 <ilokids@gmail.com>
2024-02-11 13:44:26 +0900

    Feat : 프론트엔드 추가

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import errorMiddleware from "./middleware/error.middleware.js";
-import community from "./routes/community.routes.js";
-import postlike from "./routes/postlike.js";
-import userRouter from "./routes/users.routes.js";
-import postRouter from "./routes/posts.routes.js";
-import kakaoLogin from "./routes/kakao.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import errorMiddleware from './middleware/error.middleware.js';
+import community from './routes/community.routes.js';
+import postlike from './routes/postlike.js';
+import userRouter from './routes/users.routes.js';
+import postRouter from './routes/posts.routes.js';
+import kakaoLogin from './routes/kakao.js';
 
 const app = express();
 const PORT = 3000;
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(errorMiddleware);
 app.use(express.static('public'));
-app.use("/api", [kakaoLogin, userRouter, community, postRouter, postlike]);
+app.use('/api', [kakaoLogin, userRouter, community, postRouter, postlike]);
 
 app.listen(PORT, () => {
-  console.log(PORT, "포트로 서버 오픈");
+	console.log(PORT, '포트로 서버 오픈');
 });
